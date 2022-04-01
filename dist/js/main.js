04eccf9de81073324974a801503dac34df804fd9
David Bollyeo <david@bollyeo.com>
2022-04-01 19:27:21 +0100

    Added the serverless function

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,13 @@
 const getRandomDadJoke = async() => {
     // return "Why do ducks have webbed feet? To stamp out forest fires. Why do elephants have big feet? To stamp out burning ducks!";
-    url="https://icanhazdadjoke.com/";
-    const jokeStream = await fetch(url, {
-        headers: {
-            Accept: "application/json"
-        }
-    });
+    // url="https://icanhazdadjoke.com/";
+    // const jokeStream = await fetch(url, {
+    //     headers: {
+    //         Accept: "application/json"
+    //     }
+    // });
+    const url = "/.netlify/functions/jokes";
+    const jokeStream = await fetch(url);
     const jsonJoke = await jokeStream.json();
     const joke = jsonJoke.joke;
 
